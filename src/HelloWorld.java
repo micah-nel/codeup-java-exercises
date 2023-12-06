06ca3cc10931e09f9a446dd497f6a9049f071e88
Micah Nelthropp <micahnelthropp@gmail.com>
2023-12-06 11:40:42 -0600

    excercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,40 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello,World");
+//       int myFavoriteNumber = 6;
+//       System.out.print(myFavoriteNumber);
+//       String myString = "Hello";
+//       System.out.print(myString);
+//       float myNumber = 3.14F;
+//       System.out.print(myNumber);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//
+//        int three = (int) o;
+
+//        int x = 4;
+//        x +=5;
+//        System.out.print(x);
+
+//       int x = 3;
+//       int y = 4;
+//       y = y * x;
+//       y *= x;
+//        System.out.print(y);
+
+        int x = 10;
+        int y = 2;
+//        x = x/y;
+//        y= y-x;
+
+          x /= y;
+          System.out.print(y);
+          y -= -x;
+
+
     }
 }
